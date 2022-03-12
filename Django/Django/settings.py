b21449c84edaf28616a1923c6a35bf52a0ac8043
Zhen Luo <luo.zhe@northeastern.edu>
2022-03-12 18:20:56 -0500

    delete the secret_key

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&issp848j!if38!c-$r*z$*fow4)1(&39@idv3t2(qfr#$ey+)'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
